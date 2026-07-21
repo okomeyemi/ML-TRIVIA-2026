799ea490a04e1a3ea596ae7aff6807bae4f74d55
@okomeyemi <okomeyemi203@gmail.com>
2026-07-21 19:17:11 +0100

    update

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+// Firebase Configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEddRhDj6EHTqW8uKyFRPaqvUGeJEs9Pk",
+  authDomain: "ml-trivia-2026.firebaseapp.com",
+  projectId: "ml-trivia-2026",
+  storageBucket: "ml-trivia-2026.firebasestorage.app",
+  messagingSenderId: "1005639731202",
+  appId: "1:1005639731202:web:1e2ce5480e0b321d26ff1b",
+  measurementId: "G-GQ2K6VXBNM"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = firebase.firestore();
