b2c62a75b04926e36592f862e7796a6171f0d945
@okomeyemi <okomeyemi203@gmail.com>
2026-07-22 11:49:35 +0100

    update instructions.js

--- js/instructions.js
+++ js/instructions.js
@@ -9,3 +9,17 @@ if (!student) {
   alert("Please register before starting the quiz.");
   window.location.href = "register.html";
 }
+
+
+
+const startQuiz = document.getElementById("startQuiz");
+
+startQuiz.addEventListener("click", (e) => {
+  
+  e.preventDefault();
+  
+  localStorage.setItem("quizStartTime", Date.now());
+  
+  window.location.href = "quiz.html";
+  
+});
