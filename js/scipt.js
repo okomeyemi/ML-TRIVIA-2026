014312b1e79c8dc6aedf97eb493a4373421a7040
@okomeyemi <okomeyemi203@gmail.com>
2026-08-02 17:25:03 +0100

    update scipt.js

---
+++ js/scipt.js
@@ -0,0 +1,113 @@
+const questions = [
+  
+  {
+    question: "What is the name of Maths League's founder?",
+    options: [
+      "Akande Great",
+      "Umukoro Efe",
+      "Umukoro Valerie",
+      "None of the above"
+    ],
+    answer: 1
+  },
+  
+  {
+    question: "What is the status of Maths League founder?",
+    options: [
+      "A graduate",
+      "A student",
+      "A married man",
+      "None of the above"
+    ],
+    answer: 1
+  },
+  
+  {
+    question: "When was Maths League created?",
+    options: [
+      "19th Aug, 2026",
+      "19th Aug, 2024",
+      "19th Aug, 2025",
+      "19th Aug, 2023"
+    ],
+    answer: 1
+  },
+  
+  {
+    question: "Who is the pioneer president of Maths League?",
+    options: [
+      "Ezeh Chris",
+      "Umukoro Efe",
+      "Uwayinor Joan",
+      "Akande Great"
+    ],
+    answer: 0
+  },
+  
+  {
+    question: "Who is Akande Great in Maths League?",
+    options: [
+      "She's the VIP",
+      "She's the President",
+      "She's the PRO",
+      "She's the Admin"
+    ],
+    answer: 0
+  },
+  
+  {
+    question: "What was one of Maths League's goals toward its 2-year anniversary?",
+    options: [
+      "Over 100 members on all platforms",
+      "Organising daily challenges",
+      "Organising competitions",
+      "Having maths seminars"
+    ],
+    answer: 0
+  },
+  
+  {
+    question: "What position do you think JQ was invited into Maths League recently?",
+    options: [
+      "Director of Technology",
+      "Admin of the League",
+      "Pioneer President",
+      "Vice President"
+    ],
+    answer: 0
+  },
+  
+  {
+    question: "Which individual won Mathematician of the Year Award (2025)?",
+    options: [
+      "Charide Merit",
+      "Favour Anita",
+      "Uwayinor Joan",
+      "Ezeh Chris"
+    ],
+    answer: 2
+  },
+  
+  {
+    question: "By 19th August 2030, Maths League would be ____ years old.",
+    options: [
+      "5 years",
+      "6 years",
+      "4 years",
+      "2 years"
+    ],
+    answer: 1
+  },
+  
+  {
+    question: "What topic was the last Maths League quiz organised on?",
+    options: [
+      "Probability",
+      "Matrix",
+      "Sequences & Series",
+      "Simultaneous Equations"
+    ],
+    answer: 3
+  }
+  
+];
